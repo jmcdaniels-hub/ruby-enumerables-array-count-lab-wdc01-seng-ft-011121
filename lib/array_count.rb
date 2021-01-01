265e2df8e8824a226705b111c1178552f8e8863d
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-01 18:47:38 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,6 +2,7 @@ def count_strings(array)
   
   array.count do |element|
     element == String 
+  end  
     
   
   # Return the total number of strings in the provided array using the count enumerable
